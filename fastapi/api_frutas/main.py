8836d59d964c66b5ff4048c27a34eb3c01a12b4f
Rogério Silva <rogerio410@gmail.com>
2025-04-14 19:50:15 -0300

    API Frutas - Completo com Listas

--- fastapi/api_frutas/main.py
+++ fastapi/api_frutas/main.py
@@ -1,30 +1,45 @@
-from fastapi import FastAPI, HTTPException
+from typing import Annotated
+from fastapi import FastAPI, HTTPException, Query, status
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Fruta(BaseModel):
+  id: int
+  nome: str
+  citrica: bool
+
+
+class FrutaUpdate(BaseModel):
+  nome: str
+  citrica: bool
+
 
 frutas = [
   {'id': 1, 'nome': 'Banana', 'citrica': False}, 
   {'id': 2, 'nome': 'Maça', 'citrica': True}, 
   {'id': 3, 'nome': 'Melancia', 'citrica': False},
-  {'id': 4, 'nome': 'Acerola', 'citrica': True}
+  {'id': 4, 'nome': 'Acerola', 'citrica': True},
+  {'id': 5, 'nome': 'Pêra', 'citrica': True}
 ]
 
 @app.get('/frutas')
-def frutas_list(order: str = None, citrico: str = None):
+def frutas_list(order: str = None, 
+                citrica: Annotated[str | None, Query(max_length=1, 
+                                                     description='Filtra por cítrico')] = None):
 
   frutas_friltradas = []
-  if citrico:
+  if citrica:
     # filtra a lista de frutas e mandar somente as citricas
-    if citrico == '1':
+    if citrica == '1':
       for fruta in frutas:
         if fruta['citrica'] == True:
           frutas_friltradas.append(fruta)
-    elif citrico == '0':
+    elif citrica == '0':
       for fruta in frutas:
         if fruta['citrica'] == False:
           frutas_friltradas.append(fruta)
-    else:
-      raise HTTPException(status_code=400, detail='Cítrico deve ser 1 ou 0')
+    
   else:
     frutas_friltradas = frutas
 
@@ -52,20 +67,55 @@ def frutas_detail(id: int):
   if fruta_localizada:
     return fruta_localizada
   else:
-    raise HTTPException(status_code=404, detail='Fruta não localizada!')
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+                        detail='Fruta não localizada!')
     
 
 
-@app.post('/frutas', status_code=201)
-def frutas_create():
-  frutas.append('Manga')
-  return {'nome': 'Manga'} 
+@app.post('/frutas', status_code=status.HTTP_201_CREATED)
+def frutas_create(fruta: Fruta):
+  frutas.append({ "id": fruta.id, 
+                 "nome": fruta.nome, 
+                 "citrica": fruta.citrica})
+  return frutas[-1]
 
 
-# Utilizados
+@app.put('/frutas/{id}')
+def frutas_update(id: int, fruta: FrutaUpdate):
+  index = obter_index_fruta_por_id(id)
+
+  if not index:
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+                        detail=f'Fruta não localizada (id={id})!')
+  
+  frutas[index]['nome'] = fruta.nome
+  frutas[index]['citrica'] = fruta.citrica
+  return frutas[index]
+    
+
+@app.delete('/frutas/{id}', status_code=status.HTTP_204_NO_CONTENT)
+def frutas_remove(id: int):
+  index = obter_index_fruta_por_id(id)
+  
+  if not index:
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
+                        detail=f'Fruta não localizada (id={id})!')
+  
+  frutas.pop(index)
+
+
+
+
+# Utilidades
 def obter_fruta_por_id(id: int):
   for fruta in frutas:
     if fruta['id'] == id:
       return fruta
+    
+
+def obter_index_fruta_por_id(id: int):
+  for index, fruta in enumerate(frutas):
+    if fruta['id'] == id:
+      return index
   
   return None
